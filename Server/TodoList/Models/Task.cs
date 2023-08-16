6959354fc8dadb83eecd66c35365d51e184aef1d
sarahyasser <sarahyasserxo@gmail.com>
2023-08-16 19:55:57 +0300

    frontend structure + calling apis

---
+++ Server/TodoList/Models/Task.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoList.Models
+{
+    public class Task
+    {
+     
+        public int TaskId { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool IsCompleted { get; set; }
+   
+    }
+}
